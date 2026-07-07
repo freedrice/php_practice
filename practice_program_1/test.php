d3795bd3d443f3ca5f6ed6bb2fa7a146ff5b4aff
freedrice <friedrice729@gmail.com>
2026-07-07 15:44:29 +0800

    if-else statements

--- practice_program_1/test.php
+++ practice_program_1/test.php
@@ -1,6 +1,5 @@
-#to run php program type "php practice_program_1/test.php" in the terminal
-
 <?php
+# to run php program type "php practice_program_1/test.php" in the terminal
 
 $test_number = 67; # variable creation
 echo $test_number + 2; # echo will output the code to the terminal
@@ -9,8 +8,14 @@ $fav_number = 'sixty-seven'; #\n is used for new line
 echo "\n" . 'My favorite number is ' . $fav_number; # period is used for concatenation
 echo "\nMy favorite number is $fav_number"; #double quotes is used for directly access into variables
 
-function triangleArea($height,$base) { #function creation
+function triangleArea($height,$base) { #function name(argument, separated by comma if multiple)
     return ($height * $base)/2;
 }
 
-echo "\n" . triangleArea(6,7);
+$triangle_area = triangleArea(6,7); # wrapping functions is possible by function2(function1(argument)) function2 uses function 1 as an argument
+
+if ($triangle_area >= 21 ) { # the parentheses store the argument
+    echo "\nThe area of this triangle is 21 or greater";
+} else {
+    echo "The area of this triangle is less than 21";
+}
