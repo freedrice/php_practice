10f6d598f38995898d3713865c1143891d77c512
freedrice <friedrice729@gmail.com>
2026-07-07 16:36:11 +0800

    experimented with slugify using functions and if-else

--- practice_program_2/index.php
+++ practice_program_2/index.php
@@ -4,4 +4,18 @@ require __DIR__ . '/vendor/autoload.php'; #loading a seperate file in the vendor
 use Cocur\Slugify\Slugify;
 
 $slugify = new Slugify();
-echo $slugify->slugify('The sky is blue, and the grass is green'); # '->' will look inside an object
+echo $slugify->slugify("The sky is blue, and the grass is green"); # '->' will look inside an object
+
+# experiment with slugify
+
+function triangleArea($height,$base) {
+    return ($height * $base)/2;
+}
+
+$triangle_area = triangleArea(6,7);
+
+if ($triangle_area >= 21) {
+    echo "\n" . $slugify->slugify("The area of this triangle is greater than or equal to 21");
+} else {
+   echo "\n" . $slugify->slugify("The area of this triangle is less than 21");
+}
