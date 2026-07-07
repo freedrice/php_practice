5fb76568405bb2e7314acab674ae417d0f4d63da
freedrice <friedrice729@gmail.com>
2026-07-07 15:19:24 +0800

    string concatenations

--- practice_program_1/test.php
+++ practice_program_1/test.php
@@ -1,3 +1,7 @@
 <?php
-$test_number = 67;
-echo $test_number + 2;
+
+# $test_number = 67; variable creation
+# echo $test_number + 2; echo will output the code to the terminal
+
+$fav_number = 'sixty-seven';
+echo 'My favorite number is ' . $fav_number;
