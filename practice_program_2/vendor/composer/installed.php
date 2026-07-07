c363276e8d07d39f35644ce3d8daf64f836fd971
freedrice <friedrice729@gmail.com>
2026-07-07 16:19:30 +0800

    used composer for slugify

---
+++ practice_program_2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '237d66d6a2de13c915da200277503e33159cc5e6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '237d66d6a2de13c915da200277503e33159cc5e6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cocur/slugify' => array(
+            'pretty_version' => 'v4.7.1',
+            'version' => '4.7.1.0',
+            'reference' => 'a860dab2b9f5f37775fc6414d4f049434848165f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cocur/slugify',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
