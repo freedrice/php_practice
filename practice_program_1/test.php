d56da524d83381d7aecf96bae48f9cac45924d90
freedrice <friedrice729@gmail.com>
2026-07-07 15:27:49 +0800

    fixed the lines of code to output all echoes

--- practice_program_1/test.php
+++ practice_program_1/test.php
@@ -1,7 +1,10 @@
+#to run php program type "php practice_program_1/test.php" in the terminal
+
 <?php
 
-# $test_number = 67; variable creation
-# echo $test_number + 2; echo will output the code to the terminal
+$test_number = 67; # variable creation
+echo $test_number + 2; # echo will output the code to the terminal
 
-$fav_number = 'sixty-seven';
-echo 'My favorite number is ' . $fav_number;
+$fav_number = 'sixty-seven'; #\n is used for new line
+echo "\n" . 'My favorite number is ' . $fav_number; # period is used for concatenation
+echo "\nMy favorite number is $fav_number"; #double quotes is used for directly access into variables
