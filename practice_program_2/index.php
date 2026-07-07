727f984ca68c99156f525a31285c37a08be18362
freedrice <friedrice729@gmail.com>
2026-07-07 16:29:11 +0800

    demonstrates the slugify function

--- practice_program_2/index.php
+++ practice_program_2/index.php
@@ -2,3 +2,6 @@
 
 require __DIR__ . '/vendor/autoload.php'; #loading a seperate file in the vendor folder
 use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+echo $slugify->slugify('The sky is blue, and the grass is green'); # '->' will look inside an object
