5b525097231f6824f7e5b25f0c72cd276c8bfee2
freedrice <friedrice729@gmail.com>
2026-07-08 17:17:50 +0800

    added edit and delete buttons

--- laravel_projects/crud_practice_web/resources/views/home.blade.php
+++ laravel_projects/crud_practice_web/resources/views/home.blade.php
@@ -29,6 +29,11 @@
         <div style="background-color: gray; padding: 10px; margin: 10px;">
             <h3>{{$post['title']}}</h3>
             {{$post['body']}}
+            <p><a href="/edit-post/{{$post->id}}">Edit post</a></p>
+            <form action="/delete-post/{{$post->id}}" method="POST">
+                @method('DELETE')
+                <button>Delete</button>
+            </form>
         </div>
         @endforeach
     </div>
