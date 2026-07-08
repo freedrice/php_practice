dd57630a19b003aef79ef7afeb89f333ef2d91bf
freedrice <friedrice729@gmail.com>
2026-07-08 16:00:49 +0800

    made the registration appear in the database

--- laravel_projects/crud_practice_web/app/Http/Controllers/UserController.php
+++ laravel_projects/crud_practice_web/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -12,6 +13,10 @@ class UserController extends Controller
             'email' => ['required', 'email'],
             'password' => ['required', 'min:8']
         ]);
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        User::create($incomingFields);
+
         return 'the controller works';
     }
 }
